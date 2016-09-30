7334bfb73c303695dfd8a1809c9063a40eb87cea
marhoily <marhoily@gmail.com>
2016-09-30 12:42:22 +0300

    all tests pass; move to string works

--- src/moves.rs
+++ src/moves.rs
@@ -4,28 +4,30 @@ use piece_types;
 use piece_types::*;
 use colored_squares::*;
 
-const MOVE_FROM_MASK: u16 = 0b0000_0000_0000_1111;
-const MOVE_TO_MASK: u16 = 0b0000_0000_1111_0000;
-const MOVE_PROMOTE_TO_MASK: u16 = 0b0000_0011_0000_0000;
-
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Move(u16);
+pub struct Move(u32);
 
 impl Move {
     pub fn usual(from: Square64, to: Square64) -> Self {
         Move::with_promotion(from, to, piece_types::UNKNOWN)
     }
     pub fn with_promotion(from: Square64, to: Square64, promote_to: PieceType) -> Self {
-        Move((from.bits() as u16) | ((to.bits() as u16) << 4) | ((promote_to.bits() as u16) << 8))
+        let mut bits: u32 = 0;
+        bits |= promote_to.bits() as u32;
+        bits <<= 8;
+        bits |= to.bits() as u32;
+        bits <<= 8;
+        bits |= from.bits() as u32;
+        Move(bits)
     }
     pub fn from(self) -> Square64 {
-        Square64::new((self.0 & MOVE_FROM_MASK) as u8)
+        Square64::new(self.0 as u8)
     }
     pub fn to(self) -> Square64 {
-        Square64::new(((self.0 & MOVE_TO_MASK) >> 4) as u8)
+        Square64::new((self.0 >> 8) as u8)
     }
     pub fn promote_to(self) -> PieceType {
-        PieceType::new((((self.0 as u16) & MOVE_PROMOTE_TO_MASK) >> 8) as u8)
+        PieceType::new((self.0 >> 16) as u8)
     }
     pub fn string(self) -> String {
         let mut result = String::with_capacity(6);
@@ -53,15 +55,28 @@ mod test {
         let e2 = Square64::parse("e2");
         let e4 = Square64::parse("e4");
         let m = Move::usual(e2, e4);
-        assert_eq!(m.0, 0);
         assert_eq!(m.from().to_string(), "e2");
+        assert_eq!(m.to().to_string(), "e4");
+        assert_eq!(m.promote_to(), UNKNOWN);
     }
+
+    #[test]
+    fn promotion_move() {
+        let e2 = Square64::parse("e2");
+        let e4 = Square64::parse("e4");
+        let m = Move::with_promotion(e2, e4, QUEEN);
+        assert_eq!(m.from().to_string(), "e2");
+        assert_eq!(m.to().to_string(), "e4");
+        assert_eq!(m.promote_to(), QUEEN);
+    }
+
     #[test]
     fn usual_move_to_string() {
         let e2 = Square64::parse("e2");
         let e4 = Square64::parse("e4");
         assert_eq!(Move::usual(e2, e4).string(), "e2-e4");
     }
+
     #[test]
     fn promotion_move_to_string() {
         let e2 = Square64::parse("e2");
