8af24dd79285bc0dadba77fc308aa51ab3acd696
marhoily <marhoily@homtail.com>
2016-10-01 11:59:51 +0300

    all_squares

--- src/colored_square.rs
+++ src/colored_square.rs
@@ -36,7 +36,7 @@ impl Debug for File {
 
 impl Display for File {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "{:?}", self.char())
+        write!(f, "{}", self.char())
     }
 }
 pub mod files {
@@ -108,7 +108,7 @@ impl Debug for Rank {
 
 impl Display for Rank {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "{:?}", self.char())
+        write!(f, "{}", self.char())
     }
 }
 
@@ -130,7 +130,7 @@ impl Color {
 
 // Note that index 0 corresponds to a8, and NOT a1!
 // Indexes read left to right, top to bottom!
-#[derive(PartialEq, PartialOrd, Debug, Copy, Clone)]
+#[derive(PartialEq, PartialOrd, Copy, Clone)]
 pub struct Square(u8);
 
 impl Square {
@@ -168,6 +168,18 @@ impl Square {
         result
     }
 }
+impl Debug for Square {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{:?}", self.to_string())
+    }
+}
+
+impl Display for Square {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
 
 named!(pub parse_square(&[u8]) -> Square,
     chain!(
@@ -276,6 +288,7 @@ mod test {
     #[test]
     fn color_invert() {
         use super::Color::*;
+
         assert_eq!(White.invert(), Black);
         assert_eq!(Black.invert(), White);
     }
@@ -296,6 +309,21 @@ mod test {
         assert_eq!(
             All.into_iter().collect::<Vec<File>>(),
             [A, B, C, D, E, F, G, H]);
+    }
+
+    #[test]
+    fn all_squares() {
+        use super::squares::*;
+
+        assert_eq!(All.into_iter().collect::<Vec<Square>>(), vec!(
+            A8,B8,C8,D8,E8,F8,G8,H8,
+            A7,B7,C7,D7,E7,F7,G7,H7,
+            A6,B6,C6,D6,E6,F6,G6,H6,
+            A5,B5,C5,D5,E5,F5,G5,H5,
+            A4,B4,C4,D4,E4,F4,G4,H4,
+            A3,B3,C3,D3,E3,F3,G3,H3,
+            A2,B2,C2,D2,E2,F2,G2,H2,
+            A1,B1,C1,D1,E1,F1,G1,H1 ));
     }
 
     #[test]
@@ -350,13 +378,10 @@ mod test {
     }
 
     #[test]
-    fn print_const() {
-        println!("");
-
+    fn print_squares() {
         for s in squares::All {
             println!("pub const {} : Square = Square({});",
                      s.to_string().to_uppercase(), s.bits());
         }
-        println!("");
     }
 }
