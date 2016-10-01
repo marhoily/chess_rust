b282d5d2dd6c7f9d5d908e74704cedc1f3c531cf
marhoily <marhoily@homtail.com>
2016-10-01 11:43:13 +0300

    test all_files

--- src/colored_square.rs
+++ src/colored_square.rs
@@ -42,6 +42,15 @@ impl Display for File {
 pub mod files {
     use super::File;
 
+    pub const A: File = File(0);
+    pub const B: File = File(1);
+    pub const C: File = File(2);
+    pub const D: File = File(3);
+    pub const E: File = File(4);
+    pub const F: File = File(5);
+    pub const G: File = File(6);
+    pub const H: File = File(7);
+
     pub struct All;
 
     impl IntoIterator for All {
@@ -58,7 +67,7 @@ pub mod files {
         fn next(&mut self) -> Option<Self::Item> {
             if self.0 == 8 {
                 None
-            }else{
+            } else {
                 let result = *self;
                 self.0 += 1;
                 Some(result)
@@ -168,73 +177,73 @@ named!(pub parse_square(&[u8]) -> Square,
     );
 
 pub mod squares {
-    use super::{Square};
+    use super::Square;
     pub const FIRST: Square = Square(0);
 
-    pub const A8 : Square = Square(0);
-    pub const B8 : Square = Square(1);
-    pub const C8 : Square = Square(2);
-    pub const D8 : Square = Square(3);
-    pub const E8 : Square = Square(4);
-    pub const F8 : Square = Square(5);
-    pub const G8 : Square = Square(6);
-    pub const H8 : Square = Square(7);
-    pub const A7 : Square = Square(8);
-    pub const B7 : Square = Square(9);
-    pub const C7 : Square = Square(10);
-    pub const D7 : Square = Square(11);
-    pub const E7 : Square = Square(12);
-    pub const F7 : Square = Square(13);
-    pub const G7 : Square = Square(14);
-    pub const H7 : Square = Square(15);
-    pub const A6 : Square = Square(16);
-    pub const B6 : Square = Square(17);
-    pub const C6 : Square = Square(18);
-    pub const D6 : Square = Square(19);
-    pub const E6 : Square = Square(20);
-    pub const F6 : Square = Square(21);
-    pub const G6 : Square = Square(22);
-    pub const H6 : Square = Square(23);
-    pub const A5 : Square = Square(24);
-    pub const B5 : Square = Square(25);
-    pub const C5 : Square = Square(26);
-    pub const D5 : Square = Square(27);
-    pub const E5 : Square = Square(28);
-    pub const F5 : Square = Square(29);
-    pub const G5 : Square = Square(30);
-    pub const H5 : Square = Square(31);
-    pub const A4 : Square = Square(32);
-    pub const B4 : Square = Square(33);
-    pub const C4 : Square = Square(34);
-    pub const D4 : Square = Square(35);
-    pub const E4 : Square = Square(36);
-    pub const F4 : Square = Square(37);
-    pub const G4 : Square = Square(38);
-    pub const H4 : Square = Square(39);
-    pub const A3 : Square = Square(40);
-    pub const B3 : Square = Square(41);
-    pub const C3 : Square = Square(42);
-    pub const D3 : Square = Square(43);
-    pub const E3 : Square = Square(44);
-    pub const F3 : Square = Square(45);
-    pub const G3 : Square = Square(46);
-    pub const H3 : Square = Square(47);
-    pub const A2 : Square = Square(48);
-    pub const B2 : Square = Square(49);
-    pub const C2 : Square = Square(50);
-    pub const D2 : Square = Square(51);
-    pub const E2 : Square = Square(52);
-    pub const F2 : Square = Square(53);
-    pub const G2 : Square = Square(54);
-    pub const H2 : Square = Square(55);
-    pub const A1 : Square = Square(56);
-    pub const B1 : Square = Square(57);
-    pub const C1 : Square = Square(58);
-    pub const D1 : Square = Square(59);
-    pub const E1 : Square = Square(60);
-    pub const F1 : Square = Square(61);
-    pub const G1 : Square = Square(62);
-    pub const H1 : Square = Square(63);
+    pub const A8: Square = Square(0);
+    pub const B8: Square = Square(1);
+    pub const C8: Square = Square(2);
+    pub const D8: Square = Square(3);
+    pub const E8: Square = Square(4);
+    pub const F8: Square = Square(5);
+    pub const G8: Square = Square(6);
+    pub const H8: Square = Square(7);
+    pub const A7: Square = Square(8);
+    pub const B7: Square = Square(9);
+    pub const C7: Square = Square(10);
+    pub const D7: Square = Square(11);
+    pub const E7: Square = Square(12);
+    pub const F7: Square = Square(13);
+    pub const G7: Square = Square(14);
+    pub const H7: Square = Square(15);
+    pub const A6: Square = Square(16);
+    pub const B6: Square = Square(17);
+    pub const C6: Square = Square(18);
+    pub const D6: Square = Square(19);
+    pub const E6: Square = Square(20);
+    pub const F6: Square = Square(21);
+    pub const G6: Square = Square(22);
+    pub const H6: Square = Square(23);
+    pub const A5: Square = Square(24);
+    pub const B5: Square = Square(25);
+    pub const C5: Square = Square(26);
+    pub const D5: Square = Square(27);
+    pub const E5: Square = Square(28);
+    pub const F5: Square = Square(29);
+    pub const G5: Square = Square(30);
+    pub const H5: Square = Square(31);
+    pub const A4: Square = Square(32);
+    pub const B4: Square = Square(33);
+    pub const C4: Square = Square(34);
+    pub const D4: Square = Square(35);
+    pub const E4: Square = Square(36);
+    pub const F4: Square = Square(37);
+    pub const G4: Square = Square(38);
+    pub const H4: Square = Square(39);
+    pub const A3: Square = Square(40);
+    pub const B3: Square = Square(41);
+    pub const C3: Square = Square(42);
+    pub const D3: Square = Square(43);
+    pub const E3: Square = Square(44);
+    pub const F3: Square = Square(45);
+    pub const G3: Square = Square(46);
+    pub const H3: Square = Square(47);
+    pub const A2: Square = Square(48);
+    pub const B2: Square = Square(49);
+    pub const C2: Square = Square(50);
+    pub const D2: Square = Square(51);
+    pub const E2: Square = Square(52);
+    pub const F2: Square = Square(53);
+    pub const G2: Square = Square(54);
+    pub const H2: Square = Square(55);
+    pub const A1: Square = Square(56);
+    pub const B1: Square = Square(57);
+    pub const C1: Square = Square(58);
+    pub const D1: Square = Square(59);
+    pub const E1: Square = Square(60);
+    pub const F1: Square = Square(61);
+    pub const G1: Square = Square(62);
+    pub const H1: Square = Square(63);
 
     pub struct All;
     impl IntoIterator for All {
@@ -251,8 +260,7 @@ pub mod squares {
         fn next(&mut self) -> Option<Self::Item> {
             if self.0 == 64 {
                 None
-            }
-            else {
+            } else {
                 let copy = *self;
                 self.0 += 1;
                 Some(copy)
@@ -264,6 +272,7 @@ pub mod squares {
 #[cfg(test)]
 mod test {
     use super::*;
+    use super::files::*;
 
     #[test]
     fn color_invert() {
@@ -273,14 +282,21 @@ mod test {
 
     #[test]
     fn file_char() {
-        assert_eq!(File::new(0).char(), 'a');
-        assert_eq!(File::new(1).char(), 'b');
-        assert_eq!(File::new(2).char(), 'c');
-        assert_eq!(File::new(3).char(), 'd');
-        assert_eq!(File::new(4).char(), 'e');
-        assert_eq!(File::new(5).char(), 'f');
-        assert_eq!(File::new(6).char(), 'g');
-        assert_eq!(File::new(7).char(), 'h');
+        assert_eq!(A.char(), 'a');
+        assert_eq!(B.char(), 'b');
+        assert_eq!(C.char(), 'c');
+        assert_eq!(D.char(), 'd');
+        assert_eq!(E.char(), 'e');
+        assert_eq!(F.char(), 'f');
+        assert_eq!(G.char(), 'g');
+        assert_eq!(H.char(), 'h');
+    }
+
+    #[test]
+    fn all_files() {
+        assert_eq!(
+            All.into_iter().collect::<Vec<File>>(),
+            [A, B, C, D, E, F, G, H]);
     }
 
     #[test]
