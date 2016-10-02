dc70bd2d3dbc840ad874ad8609f37807fa031a77
marhoily <marhoily@homtail.com>
2016-10-02 19:59:25 +0300

    parse castle

--- src/castle.rs
+++ src/castle.rs
@@ -2,6 +2,8 @@
 #![allow(trivial_casts, trivial_numeric_casts)]
 
 use std::fmt::{Display, Result, Formatter};
+use nom::IResult;
+use nom::IResult::*;
 
 // todo: make castle be masks for squares that need checking?
 // todo: let's keep parser near the struct
@@ -17,6 +19,11 @@ bitflags! {
         const BQ = 1 << 3,
         const BK = 1 << 4,
         const ALL = Q.bits | K.bits,
+    }
+}
+impl Castle {
+    pub fn parse(input: &str) -> Self {
+        parse_castle(input.as_bytes()).unwrap().1
     }
 }
 
@@ -41,10 +48,70 @@ impl Display for Castle {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ParsingError {
+    Repeat,
+    UnrecognizedToken,
+}
+
+fn consume(c: char) -> Option<Castle> {
+    match c {
+        'Q' => Some(WQ),
+        'K' => Some(WK),
+        'q' => Some(BQ),
+        'k' => Some(BK),
+        '-' => Some(NONE),
+        _ => None,
+    }
+}
+
+pub fn parse_castle(input: &[u8]) -> IResult<&[u8], Castle, ParsingError> {
+    use nom::Err::Position;
+    use nom::ErrorKind::Custom;
+    use nom::Needed::Unknown;
+    use castle::ParsingError::*;
+
+    let mut result = NONE;
+    let mut consumed = 0;
+    for &e in input {
+        consumed += 1;
+        match consume(e as char) {
+            None => return Error(Position(Custom(UnrecognizedToken), &input[consumed..])),
+            Some(NONE) => break,
+            Some(c) => {
+                if result.intersects(c) {
+                    return Error(Position(Custom(Repeat), &input[consumed..]));
+                }
+                result |= c;
+            }
+        }
+    }
+    if consumed > 0 {
+        Done(&input[consumed..], result)
+    } else {
+        Incomplete(Unknown)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[test]
+    fn parse() {
+        let check = |sample: &str| {
+            let parsed = Castle::parse(sample);
+            assert_eq!(format!("{}", parsed), sample)
+        };
+        check("-");
+        check("Qk");
+        check("Kq");
+        check("Qkq");
+        check("Kkq");
+        check("KQq");
+        check("KQk");
+        check("KQkq");
+    }
     #[test]
     fn display() {
         assert_eq!(format!("{}", ALL), "KQkq");
