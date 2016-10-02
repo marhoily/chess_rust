14b9b1c3fbb2c0eae44e5a2b77aebd407d32f8fc
marhoily <marhoily@homtail.com>
2016-10-02 12:01:36 +0300

    use what is presumably faster approach for LSB: https://chessprogramming.wikispaces.com/BitScan

--- src/mask/mod.rs
+++ src/mask/mod.rs
@@ -104,8 +104,8 @@ impl Mask {
     }
     pub fn least_significant_bit(self) -> usize {
         debug_assert!(self.0 != 0);
-        let bb = self.0 & self.0.wrapping_neg();
-        LSB[(bb.wrapping_mul(MAGIC) >> 58) as usize]
+        let bb = self.0 ^ self.0.wrapping_sub(1);
+        MSB[(bb.wrapping_mul(MAGIC) >> 58) as usize]
     }
 }
 impl BitOr<Mask> for Mask {
@@ -161,12 +161,12 @@ impl Not for Mask {
 
 pub mod masks;
 
+#[allow(dead_code)]
 static LSB: &'static [usize] = &[0, 1, 48, 2, 57, 49, 28, 3, 61, 58, 50, 42, 38, 29, 17, 4, 62,
                                  55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5, 63,
                                  47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11, 46,
                                  26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6];
 
-#[allow(dead_code)]
 static MSB: &'static [usize] = &[0, 47, 1, 56, 48, 27, 2, 60, 57, 49, 41, 37, 28, 16, 3, 61, 54,
                                  58, 35, 52, 50, 42, 21, 44, 38, 32, 29, 23, 17, 11, 4, 62, 46,
                                  55, 26, 59, 40, 36, 15, 53, 34, 51, 20, 43, 31, 22, 10, 45, 25,
