db23a3b548ce5044081af5c2c6a56e7b1fab7da5
marhoily <marhoily@homtail.com>
2016-10-02 17:20:51 +0300

    cleanup

--- src/mask/masks.rs
+++ src/mask/masks.rs
@@ -132,7 +132,6 @@ impl Iterator for MaskIter {
 #[cfg(test)]
 mod test {
     use super::*;
-    use super::super::Mask;
     use itertools::*;
 
     #[test]
@@ -144,66 +143,59 @@ mod test {
                         G2, H2, A1, B1, C1, D1, E1, F1, G1, H1]);
     }
 
-    fn split(m: Mask) -> Vec<Mask> {
-        m.single_bits().collect::<Vec<_>>()
-    }
-
     #[test]
     fn files() {
-        assert_eq!(split(files::A), [A8, A7, A6, A5, A4, A3, A2, A1]);
-        assert_eq!(split(files::B), [B8, B7, B6, B5, B4, B3, B2, B1]);
-        assert_eq!(split(files::C), [C8, C7, C6, C5, C4, C3, C2, C1]);
-        assert_eq!(split(files::D), [D8, D7, D6, D5, D4, D3, D2, D1]);
-        assert_eq!(split(files::E), [E8, E7, E6, E5, E4, E3, E2, E1]);
-        assert_eq!(split(files::F), [F8, F7, F6, F5, F4, F3, F2, F1]);
-        assert_eq!(split(files::G), [G8, G7, G6, G5, G4, G3, G2, G1]);
-        assert_eq!(split(files::H), [H8, H7, H6, H5, H4, H3, H2, H1]);
+        use super::files::*;
+
+        assert_eq!(A.single_bits().collect_vec(),
+                   [A8, A7, A6, A5, A4, A3, A2, A1]);
+
+        assert_eq!(B.single_bits().collect_vec(),
+                   [B8, B7, B6, B5, B4, B3, B2, B1]);
+
+        assert_eq!(C.single_bits().collect_vec(),
+                   [C8, C7, C6, C5, C4, C3, C2, C1]);
+
+        assert_eq!(D.single_bits().collect_vec(),
+                   [D8, D7, D6, D5, D4, D3, D2, D1]);
+
+        assert_eq!(E.single_bits().collect_vec(),
+                   [E8, E7, E6, E5, E4, E3, E2, E1]);
+
+        assert_eq!(F.single_bits().collect_vec(),
+                   [F8, F7, F6, F5, F4, F3, F2, F1]);
+
+        assert_eq!(G.single_bits().collect_vec(),
+                   [G8, G7, G6, G5, G4, G3, G2, G1]);
+
+        assert_eq!(H.single_bits().collect_vec(),
+                   [H8, H7, H6, H5, H4, H3, H2, H1]);
     }
     #[test]
     fn ranks() {
-        assert_eq!(split(ranks::_8), [A8, B8, C8, D8, E8, F8, G8, H8]);
-        assert_eq!(split(ranks::_7), [A7, B7, C7, D7, E7, F7, G7, H7]);
-        assert_eq!(split(ranks::_6), [A6, B6, C6, D6, E6, F6, G6, H6]);
-        assert_eq!(split(ranks::_5), [A5, B5, C5, D5, E5, F5, G5, H5]);
-        assert_eq!(split(ranks::_4), [A4, B4, C4, D4, E4, F4, G4, H4]);
-        assert_eq!(split(ranks::_3), [A3, B3, C3, D3, E3, F3, G3, H3]);
-        assert_eq!(split(ranks::_2), [A2, B2, C2, D2, E2, F2, G2, H2]);
-        assert_eq!(split(ranks::_1), [A1, B1, C1, D1, E1, F1, G1, H1]);
-    }
+        use super::ranks::*;
+        assert_eq!(_8.single_bits().collect_vec(),
+                   [A8, B8, C8, D8, E8, F8, G8, H8]);
 
-    #[test]
-    fn print_const_files() {
-        use geometry::Square;
-        use std::ascii::AsciiExt;
+        assert_eq!(_7.single_bits().collect_vec(),
+                   [A7, B7, C7, D7, E7, F7, G7, H7]);
 
-        for file in ::geometry::files::All {
-            let mut mask = EMPTY;
-            for rank in ::geometry::ranks::All {
-                mask |= Square::from(file, rank).mask();
-            }
-            println!("pub const {} : Mask = Mask(0x{:X});",
-                     file.char().to_ascii_uppercase(),
-                     mask.bits());
-        }
-    }
-    #[test]
-    fn print_const_ranks() {
-        use geometry::Square;
+        assert_eq!(_6.single_bits().collect_vec(),
+                   [A6, B6, C6, D6, E6, F6, G6, H6]);
 
-        for rank in ::geometry::ranks::All {
-            let mut mask = EMPTY;
-            for file in ::geometry::files::All {
-                mask |= Square::from(file, rank).mask();
-            }
-            println!("pub const _{} : Mask = Mask(0x{:X});", rank, mask.bits());
-        }
-    }
-    #[test]
-    fn print_const_squares() {
-        for s in ::geometry::squares::All {
-            println!("pub const {} : Mask = Mask(0x{:X});",
-                     s.to_string().to_uppercase(),
-                     s.mask().bits());
-        }
+        assert_eq!(_5.single_bits().collect_vec(),
+                   [A5, B5, C5, D5, E5, F5, G5, H5]);
+
+        assert_eq!(_4.single_bits().collect_vec(),
+                   [A4, B4, C4, D4, E4, F4, G4, H4]);
+
+        assert_eq!(_3.single_bits().collect_vec(),
+                   [A3, B3, C3, D3, E3, F3, G3, H3]);
+
+        assert_eq!(_2.single_bits().collect_vec(),
+                   [A2, B2, C2, D2, E2, F2, G2, H2]);
+
+        assert_eq!(_1.single_bits().collect_vec(),
+                   [A1, B1, C1, D1, E1, F1, G1, H1]);
     }
 }
