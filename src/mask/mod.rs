91a4985ba25190b44038ec5aa2d9097a87d98a57
marhoily <marhoily@homtail.com>
2016-10-02 11:33:02 +0300

    started least_significant_bit

--- src/mask/mod.rs
+++ src/mask/mod.rs
@@ -101,6 +101,10 @@ impl Mask {
     pub fn has_mote_than_one_bit_set(self) -> bool {
         self.0 & (self.0.wrapping_sub(1)) != 0
     }
+    pub fn least_significant_bit(self) -> usize {
+        debug_assert!(self.0 != 0);
+        LSB[(self.0.wrapping_mul(MAGIC) >> 58) as usize]
+    }
 }
 impl BitOr<Mask> for Mask {
     type Output = Mask;
@@ -154,6 +158,19 @@ impl Not for Mask {
 }
 
 pub mod masks;
+
+static LSB: &'static [usize] = &[0, 1, 48, 2, 57, 49, 28, 3, 61, 58, 50, 42, 38, 29, 17, 4, 62,
+                                 55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5, 63,
+                                 47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11, 46,
+                                 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6];
+
+static MSB: &'static [usize] = &[0, 47, 1, 56, 48, 27, 2, 60, 57, 49, 41, 37, 28, 16, 3, 61, 54,
+                                 58, 35, 52, 50, 42, 21, 44, 38, 32, 29, 23, 17, 11, 4, 62, 46,
+                                 55, 26, 59, 40, 36, 15, 53, 34, 51, 20, 43, 31, 22, 10, 45, 25,
+                                 39, 14, 33, 19, 30, 9, 24, 13, 18, 8, 12, 7, 6, 5, 63];
+
+/// The De Bruijn multiplier.
+const MAGIC: u64 = 0x03f79d71b4cb0a89;
 
 #[cfg(test)]
 mod test {
