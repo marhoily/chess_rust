89719cc198730138e7f2140a3f09f12f5f7095df
marhoily <marhoily@homtail.com>
2016-10-02 19:35:40 +0300

    display castle

--- src/castle.rs
+++ src/castle.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(trivial_casts, trivial_numeric_casts)]
 
+use std::fmt::{Display, Result, Formatter};
+
 // todo: make castle be masks for squares that need checking?
 // todo: let's keep parser near the struct
 bitflags! {
@@ -15,5 +17,45 @@ bitflags! {
         const BQ = 1 << 3,
         const BK = 1 << 4,
         const ALL = Q.bits | K.bits,
+    }
+}
+
+impl Display for Castle {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        if self.contains(WK) {
+            try!(write!(f, "K"));
+        }
+        if self.contains(WQ) {
+            try!(write!(f, "Q"));
+        }
+        if self.contains(BK) {
+            try!(write!(f, "k"));
+        }
+        if self.contains(BQ) {
+            try!(write!(f, "q"));
+        }
+        if self.is_empty() {
+            try!(write!(f, "-"));
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn display() {
+        assert_eq!(format!("{}", ALL), "KQkq");
+        assert_eq!(format!("{}", NONE), "-");
+        assert_eq!(format!("{}", Q), "Qq");
+        assert_eq!(format!("{}", K), "Kk");
+        assert_eq!(format!("{}", W), "KQ");
+        assert_eq!(format!("{}", B), "kq");
+        assert_eq!(format!("{}", WQ), "Q");
+        assert_eq!(format!("{}", WK), "K");
+        assert_eq!(format!("{}", BQ), "q");
+        assert_eq!(format!("{}", BK), "k");
     }
 }
