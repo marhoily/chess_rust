153c3cea8c0820585d0aa9241a7ed488a80e06e4
marhoily <marhoily@homtail.com>
2016-10-03 21:27:37 +0300

    split big mask\mod.rs file up

---
+++ src/mask/iter.rs
@@ -0,0 +1,84 @@
+use super::*;
+
+#[derive(Eq, Copy, Clone, Debug, PartialEq)]
+pub struct MaskIter(pub Mask);
+impl Iterator for MaskIter {
+    type Item = Mask;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 == masks::EMPTY {
+            None
+        } else {
+            let mask = self.0;
+            let result = mask.least_significant_bit();
+            self.0 = Mask(mask.0 & mask.0.wrapping_sub(1));
+            Some(result)
+        }
+    }
+}
+impl DoubleEndedIterator for MaskIter {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.0 == masks::EMPTY {
+            None
+        } else {
+            let mask = self.0;
+            let result = mask.most_significant_bit();
+            self.0 = Mask(mask.0 ^ result.0);
+            Some(result)
+        }
+    }
+}
+
+#[derive(Eq, Copy, Clone, Debug, PartialEq)]
+pub struct IndexIter(pub Mask);
+impl Iterator for IndexIter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 == masks::EMPTY {
+            None
+        } else {
+            let mask = self.0;
+            let result = mask.index_of_least_significant_bit();
+            self.0 = Mask(mask.0 & mask.0.wrapping_sub(1));
+            Some(result)
+        }
+    }
+}
+impl DoubleEndedIterator for IndexIter {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.0 == masks::EMPTY {
+            None
+        } else {
+            let mask = self.0;
+            let result = mask.index_of_most_significant_bit();
+            self.0 = Mask(mask.0 ^ (1u64 << result));
+            debug_assert!(mask.count() > self.0.count(),
+                          "{:X}, {:X}",
+                          mask.0,
+                          (self.0).0);
+            Some(result)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+    use quickcheck::*;
+
+    quickcheck! {
+        fn count_should_match_single_bits(m : Mask) -> bool {
+                m.count() as usize== m.single_bits().count()
+        }
+        fn count_should_match_single_bit_indices(m : Mask) -> bool {
+                m.count() as usize== m.single_bit_indices().count()
+        }
+    }
+
+    impl Arbitrary for Mask {
+        fn arbitrary<G: Gen>(g: &mut G) -> Self {
+            Mask(g.next_u64())
+        }
+    }
+}
--- src/mask/mod.rs
+++ src/mask/mod.rs
@@ -121,77 +121,15 @@ impl Mask {
         Mask(bb & bb.wrapping_neg())
     }
 
-    pub fn single_bits(self) -> MaskIter {
-        MaskIter(self)
+    pub fn single_bits(self) -> iter::MaskIter {
+        iter::MaskIter(self)
     }
-    pub fn single_bit_indices(self) -> IndexIter {
-        IndexIter(self)
+    pub fn single_bit_indices(self) -> iter::IndexIter {
+        iter::IndexIter(self)
     }
 }
 pub mod ops;
-
-#[derive(Eq, Copy, Clone, Debug, PartialEq)]
-pub struct MaskIter(Mask);
-impl Iterator for MaskIter {
-    type Item = Mask;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.0 == masks::EMPTY {
-            None
-        } else {
-            let mask = self.0;
-            let result = mask.least_significant_bit();
-            self.0 = Mask(mask.0 & mask.0.wrapping_sub(1));
-            Some(result)
-        }
-    }
-}
-impl DoubleEndedIterator for MaskIter {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if self.0 == masks::EMPTY {
-            None
-        } else {
-            let mask = self.0;
-            let result = mask.most_significant_bit();
-            self.0 = Mask(mask.0 ^ result.0);
-            Some(result)
-        }
-    }
-}
-
-#[derive(Eq, Copy, Clone, Debug, PartialEq)]
-pub struct IndexIter(Mask);
-impl Iterator for IndexIter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.0 == masks::EMPTY {
-            None
-        } else {
-            let mask = self.0;
-            let result = mask.index_of_least_significant_bit();
-            self.0 = Mask(mask.0 & mask.0.wrapping_sub(1));
-            Some(result)
-        }
-    }
-}
-impl DoubleEndedIterator for IndexIter {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if self.0 == masks::EMPTY {
-            None
-        } else {
-            let mask = self.0;
-            let result = mask.index_of_most_significant_bit();
-            self.0 = Mask(mask.0 ^ (1u64 << result));
-            debug_assert!(mask.count() > self.0.count(),
-                          "{:X}, {:X}",
-                          mask.0,
-                          (self.0).0);
-            Some(result)
-        }
-    }
-}
-
+pub mod iter;
 pub mod masks;
 
 #[cfg(test)]
@@ -199,7 +137,6 @@ mod test {
     use super::*;
     use test::Bencher;
     use itertools::*;
-    use quickcheck::*;
     use rand::random;
 
     #[test]
@@ -397,15 +334,6 @@ mod test {
                     |^^^^^^^^|...");
     }
 
-    quickcheck! {
-        fn count_should_match_single_bits(m : Mask) -> bool {
-                m.count() as usize== m.single_bits().count()
-        }
-        fn count_should_match_single_bit_indices(m : Mask) -> bool {
-                m.count() as usize== m.single_bit_indices().count()
-        }
-    }
-
     #[test]
     fn has_mote_than_one_bit_set() {
         assert_eq!(masks::files::B.has_mote_than_one_bit_set(), true);
@@ -488,11 +416,7 @@ mod test {
     fn random_bits_shift(_: i32) -> (u64, u32) {
         (random::<u64>(), random::<u32>() % 64)
     }
-    impl Arbitrary for Mask {
-        fn arbitrary<G: Gen>(g: &mut G) -> Self {
-            Mask(g.next_u64())
-        }
-    }
+
     const LEFT: u64 = 1u64 << 63;
     const RIGHT: u64 = 1u64;
 }
