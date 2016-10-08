cd2aad89768778ec6d072fc4c318543cb84c1337
marhoily <marhoily@homtail.com>
2016-10-08 13:48:47 +0300

    cleanup

--- src/color.rs
+++ src/color.rs
@@ -9,26 +9,28 @@ pub enum Color {
 
 impl Color {
     pub fn invert(self) -> Self {
-        if self == Color::Black {
-            Color::White
-        } else {
-            Color::Black
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
         }
     }
     pub fn castle(self) -> Castle {
-        if self == Color::Black {
-            B
-        } else {
-            W
+        match self {
+            Color::White => W,
+            Color::Black => B,
         }
     }
+    pub fn parse(input: char) -> Self {
+        debug_assert!((input as u32) < 128, "it is not even an ASCII character!");
+        parse_color(&[input as u8]).unwrap().1
+    }
+
 }
 impl Display for Color {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        if *self == Color::White {
-            write!(f, "w")
-        } else {
-            write!(f, "b")
+        match *self {
+            Color::White => write!(f, "w"),
+            Color::Black => write!(f, "b"),
         }
     }
 }
@@ -39,13 +41,33 @@ named!(pub parse_color(&[u8]) -> Color,
 
 #[cfg(test)]
 mod test {
+    use super::*;
+    use super::Color::*;
+    use castle::*;
 
     #[test]
     fn color_invert() {
-        use super::Color::*;
-
         assert_eq!(White.invert(), Black);
         assert_eq!(Black.invert(), White);
     }
-
+    #[test]
+    fn castle() {
+        assert_eq!(White.castle(), W);
+        assert_eq!(Black.castle(), B);
+    }
+    #[test]
+    fn parse() {
+        assert_eq!(Color::parse('w'), White);
+        assert_eq!(Color::parse('b'), Black);
+    }
+    #[test]
+    fn display() {
+        assert_eq!(format!("{}", White), "w");
+        assert_eq!(format!("{}", Black), "b");
+    }
+    #[test]
+    fn debug() {
+        assert_eq!(format!("{:?}", White), "White");
+        assert_eq!(format!("{:?}", Black), "Black");
+    }
 }
