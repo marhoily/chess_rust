23f5986129d9b56429fce23a22ac682781a32e7d
marhoily <marhoily@homtail.com>
2016-10-13 23:41:38 +0300

    fix test

--- src/square88/mod.rs
+++ src/square88/mod.rs
@@ -25,7 +25,7 @@ impl Square88 {
     pub fn into_square(self) -> Square {
         Square::from(self.file(), self.rank())
     }
-    pub fn from_square(square : Square) -> Self {
+    pub fn from_square(square: Square) -> Self {
         Self::from(square.file(), square.rank())
     }
     pub fn new(bits: u8) -> Self {
@@ -100,18 +100,29 @@ fn forward() {
         }
         last = curr;
     }
-    // assert_eq!((0..120).map(|offset| ALL_SQUARES
-    //     .forward(offset))
-    //     .collect::<Vec<_>>(),
-    //     vec!( A8,B8,C8,D8,E8,F8,G8,H8,
-    //             A7,B7,C7,D7,E7,F7,G7,H7,
-    //             A6,B6,C6,D6,E6,F6,G6,H6,
-    //             A5,B5,C5,D5,E5,F5,G5,H5,
-    //             A4,B4,C4,D4,E4,F4,G4,H4,
-    //             A3,B3,C3,D3,E3,F3,G3,H3,
-    //             A2,B2,C2,D2,E2,F2,G2,H2,
-    //             A1,B1,C1,D1,E1,F1,G1,H1 ));
-    //        assert_eq !(FIRST.forward(120).is_valid(), false );
+}
+#[test]
+fn offset() {
+    assert_eq!((0..120)
+            .map(|offset| ALL_SQUARES.forward(offset))
+            .collect_vec(),vec!(
+                 A8,B8,C8,D8,E8,F8,G8,H8,
+                 A7,A7,A7,A7,A7,A7,A7,A7,
+                 A7,B7,C7,D7,E7,F7,G7,H7,
+                 A6,A6,A6,A6,A6,A6,A6,A6,
+                 A6,B6,C6,D6,E6,F6,G6,H6,
+                 A5,A5,A5,A5,A5,A5,A5,A5,
+                 A5,B5,C5,D5,E5,F5,G5,H5,
+                 A4,A4,A4,A4,A4,A4,A4,A4,
+                 A4,B4,C4,D4,E4,F4,G4,H4,
+                 A3,A3,A3,A3,A3,A3,A3,A3,
+                 A3,B3,C3,D3,E3,F3,G3,H3,
+                 A2,A2,A2,A2,A2,A2,A2,A2,
+                 A2,B2,C2,D2,E2,F2,G2,H2,
+                 A1,A1,A1,A1,A1,A1,A1,A1,
+                 A1,B1,C1,D1,E1,F1,G1,H1 ));
+
+    assert_eq!(ALL_SQUARES.forward(120).is_valid(), false);
 }
 
 #[test]
