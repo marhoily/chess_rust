f221effa22cd8aeed3fd848e18b17263f4fdd6b1
marhoily <marhoily@homtail.com>
2016-10-15 13:23:13 +0300

    expand position module

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -99,63 +99,7 @@ pub enum PositionError {
     Whitespace,
 }
 
-mod wrappers {
-    use super::*;
-    use castle::Castle;
-    use bit_board::BitBoard;
-    use color::Color;
-    use file::File;
-    use nom::Err::Position as P;
-    use nom::ErrorKind::Custom as C;
-    use super::PositionError::*;
-    type R<'a, T, X> = ::nom::IResult<&'a [u8], T, X>;
-
-    pub fn parse_bit_board(input: &[u8]) -> R<BitBoard, PositionError> {
-        ::bit_board::fen::parse_bit_board(input).map_err(|err| {
-            match err {
-                P(C(pe), x) => P(C(Board(pe)), x),
-                _ => panic!("parse_bit_board"),
-            }
-        })
-    }
-
-    pub fn parse_color(input: &[u8]) -> R<Color, PositionError> {
-        ::color::parse_color(input).map_err(|err| {
-            match err {
-                P(C(pe), x) => P(C(Active(pe)), x),
-                _ => panic!("parse_color"),
-            }
-        })
-    }
-
-    pub fn parse_castle(input: &[u8]) -> R<Castle, PositionError> {
-        ::castle::parse_castle(input).map_err(|err| {
-            match err {
-                P(C(pe), x) => P(C(Available(pe)), x),
-                _ => panic!("parse_castle"),
-            }
-        })
-    }
-
-    pub fn parse_file(input: &[u8]) -> R<File, PositionError> {
-        ::file::parse_file(input).map_err(|err| {
-            match err {
-                P(C(pe), x) => P(C(EnPassant(pe)), x),
-                _ => panic!("parse_file"),
-            }
-        })
-    }
-
-    named!(ws_inner(&[u8]) -> char, char!(' '));
-    pub fn ws(input: &[u8]) -> R<char, PositionError> {
-        ws_inner(input).map_err(|err| {
-            match err {
-                P(_, x) => P(C(Whitespace), x),
-                _ => panic!("ws"),
-            }
-        })
-    }
-}
+mod wrappers;
 
 // "8/8/8/8/8/8/8/8 w KQkq - 0 1"
 named!(pub parse_position<&[u8], Position, PositionError>,
