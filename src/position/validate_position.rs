874a8fbced1e1b7cc9c6defc7d0da69ec5875087
marhoily <marhoily@gmail.com>
2017-01-09 13:38:54 +0300

    WhitePawnsOnPromotionRank

--- src/position/validate_position.rs
+++ src/position/validate_position.rs
@@ -4,9 +4,12 @@ use super::*;
 
 #[derive(PartialEq, Debug)]
 pub enum Assessment {
-    Valid, 
-    HasNoWhiteKing, HasMoreThanOneWhiteKing,
-    HasNoBlackKing,HasMoreThanOneBlackKing
+    Valid,
+    HasNoWhiteKing,
+    HasMoreThanOneWhiteKing,
+    HasNoBlackKing,
+    HasMoreThanOneBlackKing,
+    WhitePawnsOnPromotionRank
 }
 
 impl Position {
@@ -17,10 +20,12 @@ impl Position {
             Assessment::HasNoWhiteKing
         } else if white_kings > 1 {
             Assessment::HasMoreThanOneWhiteKing
-        } else         if black_kings == 0 {
+        } else if black_kings == 0 {
             Assessment::HasNoBlackKing
         } else if black_kings > 1 {
             Assessment::HasMoreThanOneBlackKing
+        } else if self.board.pawns::<White>().0 & _8 != EMPTY {
+            Assessment::WhitePawnsOnPromotionRank
         } else {
             Assessment::Valid
         }
@@ -37,31 +42,42 @@ mod tests {
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
             Assessment::Valid);
     }
+
     #[test]
     fn has_no_white_king() {
         assert_assessment(
             "8/8/8/8/8/8/8/8 w - - 0 1",
             Assessment::HasNoWhiteKing);
     }
+
     #[test]
     fn two_white_kings() {
         assert_assessment(
             "8/K7/8/8/K7/8/8/8 w - - 0 1",
             Assessment::HasMoreThanOneWhiteKing);
     }
+
     #[test]
     fn has_no_black_king() {
         assert_assessment(
             "8/8/K7/8/8/8/8/8 w - - 0 1",
             Assessment::HasNoBlackKing);
     }
+
     #[test]
     fn two_black_kings() {
         assert_assessment(
             "8/k7/8/8/k7/8/K7/8 w - - 0 1",
             Assessment::HasMoreThanOneBlackKing);
     }
-    fn assert_assessment(fen: &str, expected :Assessment) {
+    #[test]
+    fn white_pawns_on_promotion_rank() {
+        assert_assessment(
+            "P7/8/8/8/k7/8/K7/8 w - - 0 1",
+            Assessment::WhitePawnsOnPromotionRank);
+    }
+
+    fn assert_assessment(fen: &str, expected: Assessment) {
         assert_eq!(Position::parse(fen).validate(), expected);
     }
 }
