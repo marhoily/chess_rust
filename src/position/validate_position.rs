e9bb1e4d28b4eecbbf4d1560e2b9b88a5f8ee40b
marhoily <marhoily@gmail.com>
2017-01-09 13:35:54 +0300

    check for black kings

--- src/position/validate_position.rs
+++ src/position/validate_position.rs
@@ -4,16 +4,23 @@ use super::*;
 
 #[derive(PartialEq, Debug)]
 pub enum Assessment {
-    Valid, HasNoWhiteKing, HasMoreThanOneWhiteKing
+    Valid, 
+    HasNoWhiteKing, HasMoreThanOneWhiteKing,
+    HasNoBlackKing,HasMoreThanOneBlackKing
 }
 
 impl Position {
     fn validate(&self) -> Assessment {
         let white_kings = self.board.kings::<White>().0.count();
+        let black_kings = self.board.kings::<Black>().0.count();
         if white_kings == 0 {
             Assessment::HasNoWhiteKing
         } else if white_kings > 1 {
             Assessment::HasMoreThanOneWhiteKing
+        } else         if black_kings == 0 {
+            Assessment::HasNoBlackKing
+        } else if black_kings > 1 {
+            Assessment::HasMoreThanOneBlackKing
         } else {
             Assessment::Valid
         }
@@ -41,6 +48,18 @@ mod tests {
         assert_assessment(
             "8/K7/8/8/K7/8/8/8 w - - 0 1",
             Assessment::HasMoreThanOneWhiteKing);
+    }
+    #[test]
+    fn has_no_black_king() {
+        assert_assessment(
+            "8/8/K7/8/8/8/8/8 w - - 0 1",
+            Assessment::HasNoBlackKing);
+    }
+    #[test]
+    fn two_black_kings() {
+        assert_assessment(
+            "8/k7/8/8/k7/8/K7/8 w - - 0 1",
+            Assessment::HasMoreThanOneBlackKing);
     }
     fn assert_assessment(fen: &str, expected :Assessment) {
         assert_eq!(Position::parse(fen).validate(), expected);
